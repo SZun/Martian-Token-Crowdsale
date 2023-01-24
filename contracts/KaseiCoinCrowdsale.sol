b8b812dc4b1aee476f829c48b62a2d6ffc92ddd2
Sam Zun <zunsamuel97@gmail.com>
2023-01-24 00:02:15 -0600

    completed the kaseicoin and kasiecoincrowdsale contracts

--- contracts/KaseiCoinCrowdsale.sol
+++ contracts/KaseiCoinCrowdsale.sol
@@ -11,11 +11,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
     
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
-    constructor(
-        // YOUR CODE HERE!
-    ) public Crowdsale(rate, wallet, token) {
-        // constructor can stay empty
-    }
+    constructor(uint rate, address payable wallet, KaseiCoin token) public Crowdsale(rate, wallet, token) {}
 }
 
 // /*
@@ -27,7 +23,9 @@ contract KaseiCoinCrowdsaleDeployer {
 
     // Add the constructor.
     constructor(
-       // YOUR CODE HERE!
+       string memory name,
+       string memory symbol,
+       address payable wallet
     ) public {
         // Create a new instance of the KaseiCoin contract.
         // YOUR CODE HERE!
